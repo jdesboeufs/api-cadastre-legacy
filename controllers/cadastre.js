27ae4603464206633fcd09cef8570ece27320e8d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-10-30 18:31:52 +0100

    Switch to stream implementation

--- controllers/cadastre.js
+++ controllers/cadastre.js
@@ -3,23 +3,37 @@ var _ = require('lodash');
 
 
 exports.commune = function(req, res, next) {
-    req.pgClient.query(format(`
-            SELECT id_cadastre, numero, voie_cadastre, surface, ST_AsGeoJSON(geometrie, 7) AS geom
-            FROM parcelles
-            WHERE insee_com = '%s';
-        `, req.params.communeInsee), function (err, result) {
-        req.pgEnd();
-        if (err) return next(err);
+    var query = req.pgClient.query(format(`
+        SELECT id_cadastre, numero, voie_cadastre, surface, ST_AsGeoJSON(geometrie, 7) AS geom
+        FROM parcelles
+        WHERE insee_com = '%s';`
+    , req.params.communeInsee));
 
-        return res.send({
-            type: 'FeatureCollection',
-            features: result.rows.map(function (row) {
-                return {
-                    type: 'Feature',
-                    geometry: JSON.parse(row.geom),
-                    properties: _.pick(row, 'id_cadastre', 'numero', 'voie_cadastre', 'surface')
-                };
-            })
-        });
+    query.on('error', err => {
+        req.pgEnd();
+        return next(err);
+    });
+
+    var first = true;
+
+    var onSuccess = _.once(() => {
+        res.type('json');
+        res.write('{"type": "FeatureCollection", "features": [');
+    });
+
+    query.on('row', row => {
+        onSuccess();
+        res.write((first ? '' : ',') + '{"type":"Feature","geometry":');
+        res.write(row.geom);
+        res.write(',"properties":');
+        res.write(JSON.stringify(_.pick(row, 'id_cadastre', 'numero', 'voie_cadastre', 'surface')));
+        res.write('}');
+        first = false;
+    });
+
+    query.on('end', () => {
+        req.pgEnd();
+        res.write(']}');
+        res.end();
     });
 };
