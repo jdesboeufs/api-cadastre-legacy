f7fcf47f8ffdb1f177dd116fcca54a02b0cc4cf1
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-03-30 12:21:24 +0200

    Update dependencies

--- controllers/cadastre.js
+++ controllers/cadastre.js
@@ -1,10 +1,10 @@
-var format = require('pg-format');
-var _ = require('lodash');
+const format = require('pg-format');
+const _ = require('lodash');
 
 
 // Alternative implementation for performance test purpose
-exports.communeStream = function(req, res, next) {
-    var query = req.pgClient.query(format(`
+function communeStream(req, res, next) {
+    const query = req.pgClient.query(format(`
         SELECT id_cadastre, numero, voie_cadastre, surface, ST_AsGeoJSON(geometrie, 7) AS geom
         FROM parcelles
         WHERE insee_com = '%s';`
@@ -15,16 +15,16 @@ exports.communeStream = function(req, res, next) {
         return next(err);
     });
 
-    var first = true;
+    let first = true;
 
-    var onSuccess = _.once(() => {
+    const onSuccess = _.once(() => {
         res.type('json');
         res.write('{"type": "FeatureCollection", "features": [');
     });
 
     query.on('row', row => {
         onSuccess();
-        res.write((first ? '' : ',') + '{"type":"Feature","geometry":');
+        res.write(`${first ? '' : ','}{"type":"Feature","geometry":`);
         res.write(row.geom);
         res.write(',"properties":');
         res.write(JSON.stringify(_.pick(row, 'id_cadastre', 'numero', 'voie_cadastre', 'surface')));
@@ -37,30 +37,30 @@ exports.communeStream = function(req, res, next) {
         res.write(']}');
         res.end();
     });
-};
+}
 
-exports.commune = function(req, res, next) {
+function commune(req, res, next) {
     req.pgClient.query(format(`
             SELECT id_cadastre, numero, voie_cadastre, surface, ST_AsGeoJSON(geometrie, 7) AS geom
             FROM parcelles
             WHERE insee_com = '%s';
-        `, req.params.communeInsee), function (err, result) {
+        `, req.params.communeInsee), (err, result) => {
         req.pgEnd();
         if (err) return next(err);
 
         return res.send({
             type: 'FeatureCollection',
-            features: result.rows.map(function (row) {
-                return {
-                    type: 'Feature',
-                    geometry: JSON.parse(row.geom),
-                    properties: _.pick(row, 'id_cadastre', 'numero', 'voie_cadastre', 'surface')
-                };
-            })
+            features: result.rows.map(row => ({
+                type: 'Feature',
+                geometry: JSON.parse(row.geom),
+                properties: _.pick(row, 'id_cadastre', 'numero', 'voie_cadastre', 'surface'),
+            })),
         });
     });
-};
+}
 
-exports.preview = function (req, res) {
-    res.redirect('http://umap.fluv.io/fr/map/new/?dataUrl=' + encodeURIComponent(process.env.ROOT_URL + '/commune/' + req.params.communeInsee));
-};
+function preview(req, res) {
+    res.redirect(`http://umap.fluv.io/fr/map/new/?dataUrl=${encodeURIComponent(`${process.env.ROOT_URL}/commune/${req.params.communeInsee}`)}`);
+}
+
+module.exports = { communeStream, commune, preview };
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var cors = require('cors');
-var compression = require('compression');
-var pg = require('pg').native;
-var morgan = require('morgan');
-var cadastre = require('./controllers/cadastre');
+const express = require('express');
+const cors = require('cors');
+const compression = require('compression');
+const pg = require('pg').native;
+const morgan = require('morgan');
+const cadastre = require('./controllers/cadastre');
 
-var app = express();
-var port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(compression());
 app.use(cors());
@@ -14,17 +14,17 @@ app.use(morgan(process.env.NODE_ENV === 'production' ? 'short' : 'dev'));
 
 /* Middlewares */
 function pgClient(req, res, next) {
-    pg.connect(process.env.PG_URI, function (err, client, done) {
+    pg.connect(process.env.PG_URI, (err, client, done) => {
         if (err) return next(err);
         req.pgClient = client;
         req.pgEnd = done;
-        next();
+        return next();
     });
 }
 
 function pgEnd(req, res, next) {
     if (req.pgEnd) req.pgEnd();
-    next();
+    return next();
 }
 
 /* Routes */
@@ -33,6 +33,6 @@ app.get('/commune/:communeInsee/stream', pgClient, cadastre.communeStream, pgEnd
 app.get('/commune/:communeInsee/preview', cadastre.preview);
 
 /* Ready! */
-app.listen(port, function () {
+app.listen(port, () => {
     console.log('Start listening on port %d', port);
 });
