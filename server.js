91dedab26331c1624bdaa2627d8fb4e43dd7dc87
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2016-05-01 15:34:34 +0200

    Drop pg-native

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
-const pg = require('pg').native;
+const pg = require('pg');
 const morgan = require('morgan');
 const cadastre = require('./controllers/cadastre');
 
